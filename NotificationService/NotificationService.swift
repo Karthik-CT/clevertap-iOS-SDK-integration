fccfa13242dd41ee0b79d04a14089db6991d91e7
Karthik Iyer <100284000+Karthik-CT@users.noreply.github.com>
2026-07-06 16:05:41 +0530

    Update NotificationService.swift

--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -33,10 +33,6 @@ class NotificationService: CTNotificationServiceExtension {
             CleverTap.sharedInstance()?.onUserLogin(profile)
         }
         
-        //        let profile: Dictionary<String, Any> = [
-        //            "Identity": "ioskk2",
-        //            "Email": "ioskk2@test.com",]
-        //        CleverTap.sharedInstance()?.onUserLogin(profile)
         // call to record the Notification viewed
         CleverTap.sharedInstance()?.recordNotificationViewedEvent(withData:request.content.userInfo)
         super.didReceive(request, withContentHandler: contentHandler)
